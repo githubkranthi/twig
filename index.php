84da14d9278d9acb7aa5cbdd5a90bb275c7cea41
githubkranthi <mr.kranthikumar235@gmail.com>
2018-06-26 22:11:20 +0530

    Twig 2.x

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+require 'vendor/autoload.php';
+
+$loader = new Twig_Loader_Filesystem('views');
+$twig   = new Twig_Environment($loader, ['debug' => true]);
+
+echo $twig->render('welcome.html.twig',[
+    'name' => "rama",
+    'age'  => "52"
+]);
